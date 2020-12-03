2bad1f16309a4bcc8b929e05840da3a414270ed7
SarikaKV <sarika.vm35@gmail.com>
2020-12-03 04:02:44 -0600

    Update similarityScore.py
    Added authorship comments

--- code/Classification/lib/similarityScore.py
+++ code/Classification/lib/similarityScore.py
@@ -7,6 +7,13 @@ import time
 import os,os.path
 from scipy import spatial
 
+__author__ = "Venkata Sarika Kondra"
+
+__version__ = "1.0.1"
+__maintainer__ = "Venkata Sarika Kondra"
+__email__ = "c00219805@louisiana.edu"
+
+
 class SimilarityScore:
 
     def __init__(self,**kwargs):
