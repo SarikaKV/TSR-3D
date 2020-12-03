184ecb4c43015bbc51c76eeb38be0c6d666914f5
SarikaKV <sarika.vm35@gmail.com>
2020-12-03 04:02:15 -0600

    Update pairwiseLocalAlignment.py
    Added authorship comments

--- code/Classification/lib/pairwiseLocalAlignment.py
+++ code/Classification/lib/pairwiseLocalAlignment.py
@@ -3,6 +3,12 @@ import math,numpy,ntpath,glob, csv, os
 import copy,pandas as pd,numpy as np
 from pathlib import Path
 
+__author__ = "Venkata Sarika Kondra"
+
+__version__ = "1.0.1"
+__maintainer__ = "Venkata Sarika Kondra"
+__email__ = "c00219805@louisiana.edu"
+
 
 class FeatureSelection:
 
